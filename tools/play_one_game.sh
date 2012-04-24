c0ae6faee6027060ea080c975a0368d67fe3430d
Sana Malik <heyletsdance@gmail.com>
2012-04-24 14:57:32 -0400

    Play game file

--- tools/play_one_game.sh
+++ tools/play_one_game.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-./playgame.py -e --player_seed 42 --end_wait=0.25 --verbose --log_dir game_logs --turns 1000 --map_file maps/cell_maze/cell_maze_p02_20.map "$@" "python sample_bots/python/HunterBot.py" "python ../MyBot.py"
+./playgame.py -e --player_seed 42 --end_wait=0.25 --verbose --log_dir game_logs --turns 1000 --map_file maps/cell_maze/cell_maze_p02_20.map "$@" "python ../MyBot.py" "python sample_bots/python/GreedyBot.py"
